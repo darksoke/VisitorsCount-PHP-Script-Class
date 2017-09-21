d929fcad969cbc6e1555933de320d39f9e7733d1
CrazyDane <thecatboiz@gmail.com>
2017-09-21 23:40:55 +0200

    Removed redundant variables and made a few new classes. :finnadie:
    I have removed unused variables/functions from the code and made a database interface with a implementation of mysql. Visitor data access will be handled in the VisitorDaoClass. Also made a class_lib with all the classes used.

--- engine/visitorsClass.php
+++ engine/visitorsClass.php
@@ -1,123 +1,74 @@
 <?php
-Class VisitorsCount{
-    public $totalVisitors = 0, $uniqueVisitors = 0, $visitorsTimed = 0, $timeCount = 0;
-    private $dbHost = "127.0.0.1";
-    private $dbUser = "root";
-    private $dbPassword = "";
-    private $dbDatabase = "darkcore_cms";
 
-    function __construct($count = 15){
-        // $count => how many minutes for timed visitors by default "Visitors past 15 minutes";
-        $this->set_session($count);
-        $this->totalVisitors = $this->totalVisitors();
-        $this->uniqueVisitors = $this->uniqueVisitors();
-        $this->visitorsTimed = $this->latestVisitors();
-        $this->timeCount = $count;
+Class VisitorsCount
+{
+
+    private $visitor_handler;
+
+    /**
+     * VisitorsCount constructor.
+     */
+    public function __construct()
+    {
+        $this->visitor_handler = new VisitorDaoClass();
     }
 
-    private function connect(){
-        //Establishing connection to database
-        $con = mysqli_connect($this->dbHost, $this->dbUser, $this->dbPassword, $this->dbDatabase);
-        if (!$con) {
-            printf("Connect failed: %s\n", mysqli_connect_error());
-            exit();
-        }
-        return $con;
-    }
-    private function set_session($minutesToCount){
+    public function set_session($minutesToCount)
+    {
         $ip = $this->get_client_ip();
         $ip = md5($ip);
         $this->save_session($ip, $minutesToCount);
     }
-    private function get_client_ip() {
-        $ipaddress = '';
+
+    private function save_session($session, $minutesCounter)
+    {
+        $this->visitor_handler->save_session($session, $minutesCounter);
+    }
+
+    private function get_session($session)
+    {
+        return $this->visitor_handler->get_session($session);
+    }
+
+    public function latestVisitors()
+    {
+        return $this->visitor_handler->latest_visitors();
+    }
+
+    public function uniqueVisitors()
+    {
+        return $this->visitor_handler->unique_visitors();
+    }
+
+    public function totalVisitors()
+    {
+        return $this->visitor_handler->total_visitors();
+    }
+
+    public function start_time($time)
+    {
+        return $time;
+    }
+
+    private function get_client_ip()
+    {
         if (getenv('HTTP_CLIENT_IP'))
-            $ipaddress = getenv('HTTP_CLIENT_IP');
-        else if(getenv('HTTP_X_FORWARDED_FOR'))
-            $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
-        else if(getenv('HTTP_X_FORWARDED'))
-            $ipaddress = getenv('HTTP_X_FORWARDED');
-        else if(getenv('HTTP_FORWARDED_FOR'))
-            $ipaddress = getenv('HTTP_FORWARDED_FOR');
-        else if(getenv('HTTP_FORWARDED'))
-            $ipaddress = getenv('HTTP_FORWARDED');
-        else if(getenv('REMOTE_ADDR'))
-            $ipaddress = getenv('REMOTE_ADDR');
+            return getenv('HTTP_CLIENT_IP');
+        else if (getenv('HTTP_X_FORWARDED_FOR'))
+            return getenv('HTTP_X_FORWARDED_FOR');
+        else if (getenv('HTTP_X_FORWARDED'))
+            return getenv('HTTP_X_FORWARDED');
+        else if (getenv('HTTP_FORWARDED_FOR'))
+            return getenv('HTTP_FORWARDED_FOR');
+        else if (getenv('HTTP_FORWARDED'))
+            return getenv('HTTP_FORWARDED');
+        else if (getenv('REMOTE_ADDR'))
+            return getenv('REMOTE_ADDR');
         else
-            $ipaddress = 'UNKNOWN';
-        return $ipaddress;
-    }
-    private function save_session($session, $minutesCounter){
-        $con = $this->connect();
-        $sql  ='INSERT INTO `visitors` VALUES (?, ?, 0)';
-        $date = time() + ($minutesCounter * 60);
-        if ($this->get_session($session) < time()) {
-            if ($stmt = $con->prepare($sql)) {
-                $stmt->bind_param('si', $session, $date);
-                $stmt->execute();
-            }
-        }
+            return "UNKNOWN";
     }
 
-    private function get_session($session){
-        $con = $this->connect();
-        $sql  ='SELECT `date` FROM `visitors` WHERE `session_token`=?';
-        $date = 0;
-        if ($stmt = $con->prepare($sql)) {
-            $stmt->bind_param('s', $session);
-            $stmt->execute();
-            $result = $stmt->get_result();
-            while ($data = $result->fetch_assoc())
-                $date = $data['date'];
-            $stmt->close();
-        }
-        $con->close();
-        return $date;
-    }
-
-    private function latestVisitors(){
-        $con = $this->connect();
-        $sql  ='SELECT * FROM `visitors` WHERE `date` >= ?';
-        $date = time();
-        $count = 0;
-        if ($stmt = $con->prepare($sql)) {
-            $stmt->bind_param('i', $date);
-            $stmt->execute();
-            $stmt->store_result();
-            $count = $stmt->num_rows;
-            $stmt->close();
-        }
-        $con->close();
-        return $count;
-    }
-
-    private function uniqueVisitors(){
-        $con = $this->connect();
-        $sql  ='SELECT COUNT(*) dupes FROM `visitors` GROUP BY `session_token` HAVING dupes >= 1;';
-        $count = 0;
-        if ($stmt = $con->prepare($sql)) {
-            $stmt->execute();
-            $stmt->store_result();
-            $count = $stmt->num_rows;
-            $stmt->close();
-        }
-        $con->close();
-        return $count;
-    }
-
-    private function totalVisitors(){
-        $con = $this->connect();
-        $sql  ='SELECT * FROM `visitors`;';
-        $count = 0;
-        if ($stmt = $con->prepare($sql)) {
-            $stmt->execute();
-            $stmt->store_result();
-            $count = $stmt->num_rows;
-            $stmt->close();
-        }
-        $con->close();
-        return $count;
-    }
 }
 
 $visitors = new VisitorsCount();
+$visitors->set_session(15);
